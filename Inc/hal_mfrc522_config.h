4ec49766cce4aa026937c08d939190f6d8573d85
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2022-03-22 19:08:01 +0300

    ✨ TEST_WRITE defined...

--- Inc/hal_mfrc522_config.h
+++ Inc/hal_mfrc522_config.h
@@ -2,3 +2,8 @@
  * Default timeout for SPI communication.
  */
 #define HAL_MFRC522_DEFAULT_TIMEOUT 500
+/**
+ * Checks the result of the newly written Register.
+ * Not suggested for production.
+ */
+#define TEST_WRITE 1
