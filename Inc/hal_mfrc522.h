250c83f3fc1e2b803fe9b99ea5caaa8ba1b642ea
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2022-03-22 15:31:06 +0300

    ✨ MFRC522_Reg enum defined...

--- Inc/hal_mfrc522.h
+++ Inc/hal_mfrc522.h
@@ -35,4 +35,63 @@ typedef struct {
 	SPI_HandleTypeDef *hspi;
 } MFRC522;
 
+typedef enum {
+  // Page 0...
+  CommandReg = 0x02,
+  ComlEnReg = 0x04,
+  DivlEnReg = 0x06,
+  ComIrqReg = 0x08,
+  DivIrqReg = 0x0A,
+  ErrorReg = 0x0C,
+  Status1Reg = 0x0E,
+  Status2Reg = 0x10,
+  FIFODataReg = 0x12,
+  FIFOLevelReg = 0x14,
+  WaterLevelReg = 0x16,
+  ControlReg = 0x18,
+  BitFramingReg = 0x1A,
+  CollReg = 0x1C,
+
+  // Page 1...
+  ModeReg = 0x22,
+  TxModeReg = 0x24,
+  RxModeReg = 0x26,
+  TxControlReg = 0x28,
+  TxASKReg = 0x2A,
+  TxSelReg = 0x2C,
+  RxSelReg = 0x2E,
+  RxThresholdReg = 0x30,
+  DemodReg = 0x32,
+  MfTxReg = 0x38,
+  MfRxReg = 0x3A,
+  SerialSpeedReg = 0x3E,
+
+  // Page 2...
+  CRCResultRegH = 0x42,
+  CRCResultRegL = 0x44,
+  ModWidthReg = 0x48,
+  RFCfgReg = 0x4C,
+  GsNReg = 0x4E,
+  CWGsPReg = 0x50,
+  ModGsPReg = 0x52,
+  TModeReg = 0x54,
+  TPrescalerReg = 0x56,
+  TReloadRegH = 0x58,
+  TReloadRegL = 0x5A,
+  TCounterValReg = 0x5C,
+
+  // Page 3...
+  TestSel1Reg = 0x62,
+  TestSel2Reg = 0x64,
+  TestPinEnReg = 0x66,
+  TestPinValueReg = 0x68,
+  TestBusReg = 0x6A,
+  AutoTestReg = 0x6C,
+  VersionReg = 0x6E,
+  AnalogTestReg = 0x70,
+  TestDAC1Reg = 0x72,
+  TestDAC2Reg = 0x74,
+  TestADCReg = 0x76,
+} MFRC522_Reg;
+
 #endif
