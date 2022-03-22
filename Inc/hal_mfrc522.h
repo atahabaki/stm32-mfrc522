e4abb770e158144b9ad465d28b91661c61e5e5e9
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2022-03-22 18:16:26 +0300

    🐛 instead of creating to enums for read and write, made just one...

--- Inc/hal_mfrc522.h
+++ Inc/hal_mfrc522.h
@@ -37,61 +37,110 @@ typedef struct {
 
 typedef enum {
   // Page 0...
-  CommandReg = 0x02,
-  ComlEnReg = 0x04,
-  DivlEnReg = 0x06,
-  ComIrqReg = 0x08,
-  DivIrqReg = 0x0A,
-  ErrorReg = 0x0C,
-  Status1Reg = 0x0E,
-  Status2Reg = 0x10,
-  FIFODataReg = 0x12,
-  FIFOLevelReg = 0x14,
-  WaterLevelReg = 0x16,
-  ControlReg = 0x18,
-  BitFramingReg = 0x1A,
-  CollReg = 0x1C,
+
+  // 00h Reserved reserved for future use Table 21 on page 38
+  CommandReg = 0x01, // starts and stops command execution Table 23 on page 38
+  ComlEnReg = 0x02,  // enable and disable interrupt request control bits Table
+                     // 25 on page 38
+  DivlEnReg = 0x03,  // enable and disable interrupt request control bits Table
+                     // 27 on page 39
+  ComIrqReg = 0x04,  // interrupt request bits Table 29 on page 39
+  DivIrqReg = 0x05,  // interrupt request bits Table 31 on page 40
+  ErrorReg = 0x06,   // error bits showing the error status of the last command
+                     // executed Table 33 on page 41
+  Status1Reg = 0x07, // communication status bits Table 35 on page 42
+  Status2Reg = 0x08, // receiver and transmitter status bits Table 37 on page 43
+  FIFODataReg =
+      0x09, // input and output of 64 byte FIFO buffer Table 39 on page 44
+  FIFOLevelReg =
+      0x0A, // number of bytes stored in the FIFO buffer Table 41 on page 44
+  WaterLevelReg =
+      0x0B, // level for FIFO underflow and overflow warning Table 43 on page 44
+  ControlReg = 0x0C, // miscellaneous control registers Table 45 on page 45
+  BitFramingReg =
+      0x0D,       // adjustments for bit-oriented frames Table 47 on page 46
+  CollReg = 0x0E, // bit position of the first bit-collision detected on the RF
+                  // interface Table 49 on page 46
+                  // 0Fh Reserved reserved for future use Table 51 on page 47
 
   // Page 1...
-  ModeReg = 0x22,
-  TxModeReg = 0x24,
-  RxModeReg = 0x26,
-  TxControlReg = 0x28,
-  TxASKReg = 0x2A,
-  TxSelReg = 0x2C,
-  RxSelReg = 0x2E,
-  RxThresholdReg = 0x30,
-  DemodReg = 0x32,
-  MfTxReg = 0x38,
-  MfRxReg = 0x3A,
-  SerialSpeedReg = 0x3E,
+
+  Reserved = 0x10, // reserved for future use Table 53 on page 47
+  ModeReg = 0x11,  // defines general modes for transmitting and receiving Table
+                   // 55 on page 48
+  TxModeReg =
+      0x12, // defines transmission data rate and framing Table 57 on page 48
+  RxModeReg =
+      0x13, // defines reception data rate and framing Table 59 on page 49
+  TxControlReg = 0x14, // controls the logical behavior of the antenna driver
+                       // pins TX1 and TX2 Table 61 on page 50
+  TxASKReg = 0x15, // controls the setting of the transmission modulation Table
+                   // 63 on page 51
+  TxSelReg = 0x16, // selects the internal sources for the antenna driver Table
+                   // 65 on page 51
+  RxSelReg = 0x17, // selects internal receiver settings Table 67 on page 52
+  RxThresholdReg =
+      0x18,        // selects thresholds for the bit decoder Table 69 on page 53
+  DemodReg = 0x19, // defines demodulator settings Table 71 on page 53
+  // Reserved = 0x1A, // reserved for future use Table 73 on page 54
+  // Reserved = 0x1B, // reserved for future use Table 75 on page 54
+  MfTxReg = 0x1C, // controls some MIFARE communication transmit parameters
+                  // Table 77 on page 55
+  MfRxReg = 0x1D, // controls some MIFARE communication receive parameters Table
+                  // 79 on page 55
+  // Reserved = 0x1E, // reserved for future use Table 81 on page 55
+  SerialSpeedReg = 0x1F, // selects the speed of the serial UART interface Table
+                         // 83 on page 55
 
   // Page 2...
-  CRCResultRegH = 0x42,
-  CRCResultRegL = 0x44,
-  ModWidthReg = 0x48,
-  RFCfgReg = 0x4C,
-  GsNReg = 0x4E,
-  CWGsPReg = 0x50,
-  ModGsPReg = 0x52,
-  TModeReg = 0x54,
-  TPrescalerReg = 0x56,
-  TReloadRegH = 0x58,
-  TReloadRegL = 0x5A,
-  TCounterValReg = 0x5C,
+
+  // Reserved = 0x20, // reserved for future use Table 85 on page 57
+  CRCResultRegH = 0x21, // shows the MSB and LSB values of the CRC calculation
+                        // Table 87 on page 57
+  CRCResultRegL = 0x22, // Table 89 on page 57
+  // Reserved = 0x23, // reserved for future use Table 91 on page 58
+  ModWidthReg = 0x24, // controls the ModWidth setting Table 93 on page 58
+  // Reserved = 0x25, // reserved for future use Table 95 on page 58
+  RFCfgReg = 0x26, // configures the receiver gain Table 97 on page 59
+  GsNReg = 0x27,   // selects the conductance of the antenna driver pins TX1 and
+                   // TX2 for modulation Table 99 on page 59
+  CWGsPReg = 0x28, // defines the conductance of the p-driver output during
+                   // periods of no modulation Table 101 on page 60
+  ModGsPReg = 0x29, // defines the conductance of the p-driver output during
+                    // periods of modulation Table 103 on page 60
+  TModeReg =
+      0x2A, // defines settings for the internal timer Table 105 on page 60
+  TPrescalerReg = 0x2B, // Table 107 on page 61
+  TReloadRegH =
+      0x2C, // defines the 16-bit timer reload value Table 109 on page 62
+  TReloadRegL = 0x2D,     // Table 111 on page 62
+  TCounterValRegH = 0x2E, // shows the 16-bit timer value Table 113 on page 63
+  TCounterValRegL = 0x2F, // Table 115 on page 63
 
   // Page 3...
-  TestSel1Reg = 0x62,
-  TestSel2Reg = 0x64,
-  TestPinEnReg = 0x66,
-  TestPinValueReg = 0x68,
-  TestBusReg = 0x6A,
-  AutoTestReg = 0x6C,
-  VersionReg = 0x6E,
-  AnalogTestReg = 0x70,
-  TestDAC1Reg = 0x72,
-  TestDAC2Reg = 0x74,
-  TestADCReg = 0x76,
-} MFRC522_Reg;
+
+  // 30h Reserved reserved for future use Table 117 on page 63
+  TestSel1Reg = 0x31, // general test signal configuration Table 119 on page 63
+  TestSel2Reg = 0x32, // general test signal configuration and PRBS control
+                      // Table 121 on page 64
+  TestPinEnReg =
+      0x33, // enables pin output driver on pins D1 to D7 Table 123 on page 64
+  TestPinValueReg = 0x34, // defines the values for D1 to D7 when it is used as
+                          // an I/O bus Table 125 on page 65
+  TestBusReg =
+      0x35, // shows the status of the internal test bus Table 127 on page 65
+  AutoTestReg = 0x36,   // controls the digital self test Table 129 on page 66
+  VersionReg = 0x37,    // shows the software version Table 131 on page 66
+  AnalogTestReg = 0x38, // controls the pins AUX1 and AUX2 Table 133 on page 67
+  TestDAC1Reg =
+      0x39, // defines the test value for TestDAC1 Table 135 on page 68
+  TestDAC2Reg =
+      0x3A, // defines the test value for TestDAC2 Table 137 on page 68
+  TestADCReg =
+      0x3B, // shows the value of ADC I and Q channels Table 139 on page 68
+  // 3Ch to 3Fh Reserved reserved for production tests Table 141 to Table 147 on
+  // page 69
+
+} MFRC522_Reg_Read;
 
 #endif
