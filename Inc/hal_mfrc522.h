a8f304a743b006f99f5adc4b3cacbb1b992fac14
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2022-03-22 14:32:20 +0300

    🎉  init...

---
+++ Inc/hal_mfrc522.h
@@ -0,0 +1,24 @@
+/*
+ *  STM32 MFRC522 Driver - An android application for those who aims to learn a new language.
+ *  Copyright (C) 2022 A. Taha Baki
+ *
+ *  This file is part of STM32 MFRC522 Driver.
+ *
+ *  STM32 MFRC522 Driver is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  STM32 MFRC522 Driver is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with STM32 MFRC522 Driver.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef HAL_MFRC522_H
+#define HAL_MFRC522_H
+
+#endif
