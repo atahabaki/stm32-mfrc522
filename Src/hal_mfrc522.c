07482a426b2e5f92a3f02c5483760e01c0349c3c
A. Taha Baki <54471918+atahabaki@users.noreply.github.com>
2022-03-24 18:28:55 +0300

    💥 using u8 instead of uint8_t...

--- Src/hal_mfrc522.c
+++ Src/hal_mfrc522.c
@@ -20,29 +20,29 @@
 
 #include <hal_mfrc522.h>
 
-MFRC522_Status HAL_MFRC522_WriteRegister(MFRC522 *rfid, MFRC522_Reg addr, uint8_t data) {
+MFRC522_Status HAL_MFRC522_WriteRegister(MFRC522 *rfid, MFRC522_Reg addr, u8 data) {
 	HAL_GPIO_WritePin(rfid->ss_pin.Port, rfid->ss_pin.Pin, GPIO_PIN_RESET); // Select the MFRC522 chip..
-  uint8_t _addr = addr << 1;
+  u8 _addr = addr << 1;
 	HAL_SPI_Transmit(rfid->hspi, &_addr, 1, HAL_MFRC522_DEFAULT_TIMEOUT); // Send address first...
 	HAL_SPI_Transmit(rfid->hspi, &data, 1, HAL_MFRC522_DEFAULT_TIMEOUT); // Then, send the data...
 	HAL_GPIO_WritePin(rfid->ss_pin.Port, rfid->ss_pin.Pin, GPIO_PIN_SET); // Disable the MFRC522 chip..
 	return RC522_OK;
 }
 
-MFRC522_Status HAL_MFRC522_WriteRegister_Multi(MFRC522 *rfid, MFRC522_Reg addr, uint16_t count, uint8_t *data) {
+MFRC522_Status HAL_MFRC522_WriteRegister_Multi(MFRC522 *rfid, MFRC522_Reg addr, uint16_t count, u8 *data) {
 	HAL_GPIO_WritePin(rfid->ss_pin.Port, rfid->ss_pin.Pin, GPIO_PIN_RESET); // Select the MFRC522 chip..
-  uint8_t _addr = addr << 1;
+  u8 _addr = addr << 1;
 	HAL_SPI_Transmit(rfid->hspi, &_addr, 1, HAL_MFRC522_DEFAULT_TIMEOUT); // Send address first...
 	HAL_SPI_Transmit(rfid->hspi, data, count, HAL_MFRC522_DEFAULT_TIMEOUT); // Then, send the data...
 	HAL_GPIO_WritePin(rfid->ss_pin.Port, rfid->ss_pin.Pin, GPIO_PIN_SET); // Disable the MFRC522 chip..
 	return RC522_OK;
 }
 
-uint8_t HAL_MFRC522_ReadRegister(MFRC522 *rfid, MFRC522_Reg addr) {
+u8 HAL_MFRC522_ReadRegister(MFRC522 *rfid, MFRC522_Reg addr) {
 	HAL_GPIO_WritePin(rfid->ss_pin.Port, rfid->ss_pin.Pin, GPIO_PIN_RESET); // Select the MFRC522 chip..
 	//HAL_SPI_Transmit(rfid->hspi, &addr, 1, HAL_MFRC522_DEFAULT_TIMEOUT); // Send address first...
-	uint8_t data[2];
-  uint8_t _addr = (addr << 1 )| 0x80;
+	u8 data[2];
+  u8 _addr = (addr << 1 )| 0x80;
 	data[0] = _addr;
 	HAL_SPI_Receive(rfid->hspi, data, 2, HAL_MFRC522_DEFAULT_TIMEOUT); // Then, send the data...
 	HAL_GPIO_WritePin(rfid->ss_pin.Port, rfid->ss_pin.Pin, GPIO_PIN_SET); // Disable the MFRC522 chip..
@@ -67,7 +67,7 @@ void HAL_MFRC522_ReadRegister_Multi(MFRC522 *rfid, MFRC522_Reg addr, u8 count, u
       for (u8 i = rxAlign; i <= 7; i++)
         mask |= (1 << i);
       // Read value and tell that we want to read the same address again.
-      // uint8_t value = m_SPI.write(address);
+      // u8 value = m_SPI.write(address);
       u8 value;
       HAL_SPI_TransmitReceive(hspi, &address, &value, 1, 500);
       // Apply mask to both current value of values[0] and the new data in value.
@@ -88,8 +88,8 @@ MFRC522_Status HAL_MFRC522_Reset(MFRC522 *rfid) {
 	// 1. Write "SoftReset" into CommandReg register...
 	HAL_MFRC522_WriteRegister(rfid, CommandReg, SoftReset|0x10);
 	// 2. check PowerDownBit is set to 1...
-	uint8_t data;
-	uint8_t countTries = 0;
+	u8 data;
+	u8 countTries = 0;
   while (countTries < 3) {
 		data = HAL_MFRC522_ReadRegister(rfid, CommandReg);
     if (data == 16)
@@ -105,7 +105,7 @@ MFRC522_Status HAL_MFRC522_Reset(MFRC522 *rfid) {
 MFRC522_Status HAL_MFRC522_SoftPowerDown(MFRC522 *rfid) {
   HAL_MFRC522_WriteRegister(rfid, CommandReg, 0x10);
   #if TEST_WRITE
-    uint8_t is_powered_down = HAL_MFRC522_ReadRegister(rfid, CommandReg);
+    u8 is_powered_down = HAL_MFRC522_ReadRegister(rfid, CommandReg);
     if ((is_powered_down & 0x10) != 0x10)
       return RC522_ERR;
   #endif
@@ -113,10 +113,10 @@ MFRC522_Status HAL_MFRC522_SoftPowerDown(MFRC522 *rfid) {
 }
 
 MFRC522_Status HAL_MFRC522_SoftPowerUp(MFRC522 *rfid) {
-  uint8_t val = HAL_MFRC522_ReadRegister(rfid, CommandReg);
+  u8 val = HAL_MFRC522_ReadRegister(rfid, CommandReg);
   val &= ~0x10;
   HAL_MFRC522_WriteRegister(rfid, CommandReg, val);
-  uint8_t is_powered_down = HAL_MFRC522_ReadRegister(rfid, CommandReg);
+  u8 is_powered_down = HAL_MFRC522_ReadRegister(rfid, CommandReg);
   uint32_t timeout = HAL_GetTick() + HAL_MFRC522_DEFAULT_TIMEOUT;
   while (HAL_GetTick() <= timeout) {
     if (!(is_powered_down & 0x10)) {
@@ -126,21 +126,21 @@ MFRC522_Status HAL_MFRC522_SoftPowerUp(MFRC522 *rfid) {
   return RC522_TIMEOUT;
 }
 
-MFRC522_Status HAL_MFRC522_SetBitMask(MFRC522 *rfid, MFRC522_Reg addr, uint8_t mask) {
-	uint8_t tmp = HAL_MFRC522_ReadRegister(rfid, addr);
+MFRC522_Status HAL_MFRC522_SetBitMask(MFRC522 *rfid, MFRC522_Reg addr, u8 mask) {
+	u8 tmp = HAL_MFRC522_ReadRegister(rfid, addr);
 	return HAL_MFRC522_WriteRegister(rfid, addr, tmp | mask);
 }
 
-MFRC522_Status HAL_MFRC522_ClearBitMask(MFRC522 *rfid, MFRC522_Reg addr, uint8_t mask) {
-	uint8_t tmp = HAL_MFRC522_ReadRegister(rfid, addr);
+MFRC522_Status HAL_MFRC522_ClearBitMask(MFRC522 *rfid, MFRC522_Reg addr, u8 mask) {
+	u8 tmp = HAL_MFRC522_ReadRegister(rfid, addr);
 	return HAL_MFRC522_WriteRegister(rfid, addr, tmp & (~mask));
 }
 
-uint8_t HAL_MFRC522_GetAntennaGain(MFRC522 *rfid) {
+u8 HAL_MFRC522_GetAntennaGain(MFRC522 *rfid) {
   return HAL_MFRC522_ReadRegister(rfid, RFCfgReg) & (0x07<<4);
 }
 
-MFRC522_Status HAL_MFRC522_SetAntennaGain(MFRC522 *rfid, uint8_t mask) {
+MFRC522_Status HAL_MFRC522_SetAntennaGain(MFRC522 *rfid, u8 mask) {
   if (HAL_MFRC522_GetAntennaGain(rfid) != mask) {
     HAL_MFRC522_ClearBitMask(rfid, RFCfgReg, (0x07<<4));
     HAL_MFRC522_SetBitMask(rfid, RFCfgReg, mask & (0x07<<4));
@@ -150,12 +150,12 @@ MFRC522_Status HAL_MFRC522_SetAntennaGain(MFRC522 *rfid, uint8_t mask) {
 
 MFRC522_Status HAL_MFRC522_Open_Antenna(MFRC522 *rfid) {
 	#if TEST_WRITE
-		uint8_t _prev = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
+		u8 _prev = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
 	#endif
 	MFRC522_Status status = HAL_MFRC522_SetBitMask(rfid, TxControlReg, 0x03);
 	#if TEST_WRITE
-		uint8_t _after = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
-		uint8_t _should = _prev | 0x03;
+		u8 _after = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
+		u8 _should = _prev | 0x03;
 		if (_after != _should)
 			return RC522_ERR;
 	#endif
@@ -164,12 +164,12 @@ MFRC522_Status HAL_MFRC522_Open_Antenna(MFRC522 *rfid) {
 
 MFRC522_Status HAL_MFRC522_Close_Antenna(MFRC522 *rfid) {
 	#if TEST_WRITE
-		uint8_t _prev = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
+		u8 _prev = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
 	#endif
 	MFRC522_Status status = HAL_MFRC522_ClearBitMask(rfid, TxControlReg, 0x03);
 	#if TEST_WRITE
-		uint8_t _after = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
-		uint8_t _should = _prev & ~(0x03);
+		u8 _after = HAL_MFRC522_ReadRegister(rfid, TxControlReg);
+		u8 _should = _prev & ~(0x03);
 		if (_after != _should)
 			return RC522_ERR;
 	#endif
@@ -231,20 +231,20 @@ MFRC522_Status HAL_MFRC522_Init(MFRC522 *rfid) {
 	HAL_MFRC522_Open_Antenna(rfid);
 	HAL_Delay(4);
 
-	uint8_t version = HAL_MFRC522_ReadRegister(rfid, VersionReg);
+	u8 version = HAL_MFRC522_ReadRegister(rfid, VersionReg);
 	if (version == 0x91 || version == 0x92)
 		return RC522_OK;
 	else return RC522_UNKNOWN_BOARD;
 }
 
-MFRC522_Status HAL_MFRC522_CalculateCRC(MFRC522 *rfid, uint8_t *data, uint8_t length, uint8_t *result) {
+MFRC522_Status HAL_MFRC522_CalculateCRC(MFRC522 *rfid, u8 *data, u8 length, u8 *result) {
   HAL_MFRC522_WriteRegister(rfid, CommandReg, Idle);
   HAL_MFRC522_WriteRegister(rfid, DivIrqReg, 0x04);
   HAL_MFRC522_WriteRegister(rfid, FIFOLevelReg, 0x80);
   HAL_MFRC522_WriteRegister_Multi(rfid, FIFODataReg, length, data);
   HAL_MFRC522_WriteRegister(rfid, CommandReg, CalcCRC);
   for (uint16_t i = 5000; i > 0; i--) {
-    uint8_t n = HAL_MFRC522_ReadRegister(rfid, DivIrqReg);
+    u8 n = HAL_MFRC522_ReadRegister(rfid, DivIrqReg);
     if (n & 0x04) {
       // Stop calculation...
       HAL_MFRC522_WriteRegister(rfid, CommandReg, Idle);
@@ -256,9 +256,9 @@ MFRC522_Status HAL_MFRC522_CalculateCRC(MFRC522 *rfid, uint8_t *data, uint8_t le
   return RC522_TIMEOUT;
 }
 
-MFRC522_Status HAL_MFRC522_CommunicatePICC(MFRC522 *rfid, uint8_t command, uint8_t waitIRq, uint8_t *sendData, uint8_t sendLen, uint8_t *backData, uint8_t *backLen, uint8_t *validBits, uint8_t rxAlign, bool checkCRC) {
-  uint8_t txLastBits = validBits ? *validBits : 0;
-  uint8_t bitFraming = (rxAlign << 4)+txLastBits;
+MFRC522_Status HAL_MFRC522_CommunicatePICC(MFRC522 *rfid, u8 command, u8 waitIRq, u8 *sendData, u8 sendLen, u8 *backData, u8 *backLen, u8 *validBits, u8 rxAlign, bool checkCRC) {
+  u8 txLastBits = validBits ? *validBits : 0;
+  u8 bitFraming = (rxAlign << 4)+txLastBits;
 
   HAL_MFRC522_WriteRegister(rfid, CommandReg, Idle);      // Stop any active command.
   HAL_MFRC522_WriteRegister(rfid, ComIrqReg, 0x7F);          // Clear all seven interrupt request bits
@@ -271,7 +271,7 @@ MFRC522_Status HAL_MFRC522_CommunicatePICC(MFRC522 *rfid, uint8_t command, uint8
 
   uint16_t i;
   for(i = 2000; i > 0; i--) {
-    uint8_t n = HAL_MFRC522_ReadRegister(rfid, ComIrqReg);  // ComIrqReg[7..0] bits are: Set1 TxIRq RxIRq IdleIRq HiAlertIRq LoAlertIRq ErrIRq TimerIRq
+    u8 n = HAL_MFRC522_ReadRegister(rfid, ComIrqReg);  // ComIrqReg[7..0] bits are: Set1 TxIRq RxIRq IdleIRq HiAlertIRq LoAlertIRq ErrIRq TimerIRq
     if(n & waitIRq)          // One of the interrupts that signal success has been set.
       break;
     if(n & 0x01)            // Timer interrupt - nothing received in 25ms
@@ -282,15 +282,15 @@ MFRC522_Status HAL_MFRC522_CommunicatePICC(MFRC522 *rfid, uint8_t command, uint8
     return RC522_TIMEOUT;
   
   // Stop now if any errors except collisions were detected.
-  uint8_t errorRegValue = HAL_MFRC522_ReadRegister(rfid, ErrorReg); // ErrorReg[7..0] bits are: WrErr TempErr reserved BufferOvfl CollErr CRCErr ParityErr ProtocolErr
+  u8 errorRegValue = HAL_MFRC522_ReadRegister(rfid, ErrorReg); // ErrorReg[7..0] bits are: WrErr TempErr reserved BufferOvfl CollErr CRCErr ParityErr ProtocolErr
   if(errorRegValue & 0x13)   // BufferOvfl ParityErr ProtocolErr
     return RC522_ERR;
   
-  uint8_t _validBits = 0;
+  u8 _validBits = 0;
   
   // If the caller wants data back, get it from the MFRC522.
   if(backData && backLen) {
-    uint8_t n = HAL_MFRC522_ReadRegister(rfid, FIFOLevelReg);  // Number of bytes in the FIFO
+    u8 n = HAL_MFRC522_ReadRegister(rfid, FIFOLevelReg);  // Number of bytes in the FIFO
     if(n > *backLen) {
       return RC522_NO_ROOM;
     }
@@ -314,7 +314,7 @@ MFRC522_Status HAL_MFRC522_CommunicatePICC(MFRC522 *rfid, uint8_t command, uint8
     if(*backLen < 2 || _validBits != 0)
       return RC522_CRC_WRONG;
     // Verify CRC_A - do our own calculation and store the control in controlBuffer.
-    uint8_t controlBuffer[2];
+    u8 controlBuffer[2];
     MFRC522_Status status = HAL_MFRC522_CalculateCRC(rfid, &backData[0], *backLen-2, &controlBuffer[0]);
     if(status != RC522_OK)
       return status;
@@ -325,7 +325,7 @@ MFRC522_Status HAL_MFRC522_CommunicatePICC(MFRC522 *rfid, uint8_t command, uint8
   return RC522_OK;
 }
 
-MFRC522_Status HAL_MFRC522_TransceiveData(MFRC522 *rfid, uint8_t *sendData, uint8_t sendLen, uint8_t *backData, uint8_t *backLen, uint8_t *validBits, uint8_t rxAlign, bool checkCRC) {
-  uint8_t waitIRq = 0x30;
+MFRC522_Status HAL_MFRC522_TransceiveData(MFRC522 *rfid, u8 *sendData, u8 sendLen, u8 *backData, u8 *backLen, u8 *validBits, u8 rxAlign, bool checkCRC) {
+  u8 waitIRq = 0x30;
   return HAL_MFRC522_CommunicatePICC(rfid, Transceive, waitIRq, sendData, sendLen, backData, backLen, validBits, rxAlign, checkCRC);
 }
